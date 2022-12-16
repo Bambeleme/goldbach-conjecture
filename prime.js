faf1e1147fa804f04fc56d624bdc9ef87c731d19
Bambeleme <daniel.mittmann@gmx.de>
2022-12-16 23:43:36 +0100

    JS CODE 2x

--- prime.js
+++ prime.js
@@ -5,30 +5,33 @@ const isPrime = (n) => {
     return true;
 }
 
-console.log(isPrime(70));
-console.log(isPrime(23));
+//console.log(isPrime(70));
+//console.log(isPrime(23));
 
 const listprimes = (limit) => {
     console.log("Berechnung der Primzahlen bis " + limit)
-    n = [0, 0] 
+    n = [0, 0, 1, 1] 
     c = 0
-    while (n.length <= limit) {
-        n.push(1);    
+    while (n.length <= limit-1) {
+        n.push(0, 1);            
     }
-    for (let i = 2; i <= limit**(0.5); i++) {
+    
+    for (let i = 1; i <= limit**(0.5)+1; i++) {
         if (n[i] != 0) {
             for (let j = i * i; j < limit + 1; j+=i){
                 n[j] = 0
             }            
         }
     }
-    for (let i = 1; i < n.length; i++){
+    for (let i = 2; i <= limit; i++){
         if (n[i]!=0){
             c += 1
             p = i
+            //console.log("nummer " + c + " " + n + " Länge: " + n.length)
+            //console.log(p + " ist Primzahl bis " +i)
         }
     }
     console.log("Es gibt " + c + " Primzahlen im Bereich bis "+ limit)
-    return true;
+    return n;
 }
-console.log(listprimes(10**8));
+console.log(listprimes(5));
